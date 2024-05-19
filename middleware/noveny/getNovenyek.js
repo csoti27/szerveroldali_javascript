c23b84655f8973e5215fd9834548286bd963e9a7
csoti27 <100168321+csoti27@users.noreply.github.com>
2024-05-19 20:48:58 +0200

    2

--- middleware/noveny/getNovenyek.js
+++ middleware/noveny/getNovenyek.js
@@ -8,17 +8,14 @@ const requireOption = require('../common/requireOption');
     const NovenyModel = requireOption(objectrepository, 'NovenyModel');
 
     return function(req, res, next) {
-        if (typeof res.locals.botanikus === 'undefined') {
-            return next();
-        }
 
-        NovenyModel.find({ _botanikus: res.locals.botanikus._id }, (err, results) => {
-            if (err) {
-                return next(err);
-            }
 
-            res.locals.novenyek = results;
-            return next();
-        });
+        NovenyModel.find({})
+        .then(noveny => {
+             res.locals.noveny = noveny
+            }).catch(err=>{
+                console.log(err);
+                return res.status(500).json({message:'error'});
+            });
     };
  };
